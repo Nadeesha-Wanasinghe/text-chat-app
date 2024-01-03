296d2e0616a2b97f0a1a69a48d2bdb3475454c33
nadeesha <nadeeshalakshan525@gmail.com>
2024-01-03 18:25:41 +0530

    Finish the UI design

---
+++ text-chat-app-client/src/main/java/lk/ijse/dep11/app/client/controller/LoginFormController.java
@@ -0,0 +1,15 @@
+package lk.ijse.dep11.app.client.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginFormController {
+    public AnchorPane root;
+    public TextField txtName;
+    public Button btnJoin;
+
+    public void btnJoinOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ text-chat-app-client/src/main/java/lk/ijse/dep11/app/client/controller/MainFormController.java
@@ -0,0 +1,17 @@
+package lk.ijse.dep11.app.client.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class MainFormController {
+    public TextField txtMessage;
+    public TextArea txtDisplay;
+    public Button btnSend;
+    public Label lblUser;
+
+    public void btnSendOnAction(ActionEvent actionEvent) {
+    }
+}
