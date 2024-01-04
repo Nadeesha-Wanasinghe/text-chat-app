b50adc4a8ea41a81c8975b573748bc627492a274
nadeesha <nadeeshalakshan525@gmail.com>
2024-01-04 16:58:54 +0530

    Finalize the client app

--- text-chat-app-client/src/main/java/lk/ijse/dep11/app/client/controller/LoginFormController.java
+++ text-chat-app-client/src/main/java/lk/ijse/dep11/app/client/controller/LoginFormController.java
@@ -1,15 +1,32 @@
 package lk.ijse.dep11.app.client.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginFormController {
     public AnchorPane root;
     public TextField txtName;
     public Button btnJoin;
 
-    public void btnJoinOnAction(ActionEvent actionEvent) {
+    public void btnJoinOnAction(ActionEvent actionEvent) throws IOException {
+        if (txtName.getText().isBlank()) return;
+
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/MainForm.fxml"));
+        Scene mainScene = new Scene(fxmlLoader.load());
+        MainFormController controller = fxmlLoader.getController();
+        controller.initData(txtName.getText());
+        stage.setScene(mainScene);
+        stage.setTitle("Text Chat App");
+        stage.show();
+        stage.centerOnScreen();
+        ((Stage)(root.getScene().getWindow())).close();
     }
 }
--- text-chat-app-client/src/main/java/lk/ijse/dep11/app/client/controller/MainFormController.java
+++ text-chat-app-client/src/main/java/lk/ijse/dep11/app/client/controller/MainFormController.java
@@ -1,17 +1,64 @@
 package lk.ijse.dep11.app.client.controller;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
 public class MainFormController {
     public TextField txtMessage;
     public TextArea txtDisplay;
     public Button btnSend;
     public Label lblUser;
+    private Socket remoteSocket;
 
-    public void btnSendOnAction(ActionEvent actionEvent) {
+    public void initialize(){
+        new Thread(()->{
+            try {
+                remoteSocket = new Socket("localhost", 5050);
+                InputStream is = remoteSocket.getInputStream();
+                BufferedInputStream bis = new BufferedInputStream(is);
+                while (true){
+                    byte[] buffer =new byte[1024];
+                    int read = bis.read(buffer);
+                    if (read == -1) break;
+                    Platform.runLater(()->{
+                        txtDisplay.setText(new String(buffer, 0, read));
+                    });
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+    }
+
+    public void initData(String user){
+        lblUser.setText(user);
+        try {
+            writeMessage("Entered: " + lblUser.getText() + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void btnSendOnAction(ActionEvent actionEvent) throws IOException {
+        if (txtMessage.getText().isBlank()) return;
+        writeMessage(lblUser.getText() + ": " + txtMessage.getText() + "\n");
+        txtMessage.clear();
+        txtMessage.requestFocus();
+    }
+
+    private void writeMessage(String message) throws IOException {
+        BufferedOutputStream bos = new BufferedOutputStream(remoteSocket.getOutputStream());
+        bos.write(message.getBytes());
+        bos.flush();
     }
 }
