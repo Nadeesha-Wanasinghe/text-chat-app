0e3d46635a36a066fbe09913cc72d2c060c9c7a5
nadeesha <nadeeshalakshan525@gmail.com>
2024-01-08 00:18:33 +0530

    Implement notifyClients

--- text-chat-app-server/src/main/java/ServerAppInitializer.java
+++ text-chat-app-server/src/main/java/ServerAppInitializer.java
@@ -1,8 +1,12 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ServerAppInitializer {
+
+    private final static List<Socket> clientList = new ArrayList<>();
     private static String messages = "";
 
     public static void main(String[] args) throws IOException {
@@ -15,6 +19,8 @@ public class ServerAppInitializer {
                 Socket localSocket = serverSocket.accept();
 
                 new Thread(() -> {
+                    clientList.add(localSocket);
+
                     try {
                         OutputStream os = localSocket.getOutputStream();
                         BufferedOutputStream bos = new BufferedOutputStream(os);
@@ -29,7 +35,10 @@ public class ServerAppInitializer {
                             int read = bis.read(buffer);
                             if (read == -1) throw new EOFException();
                             messages += new String(buffer, 0, read);
+                            notifyClients();
                         }
+                    }catch (EOFException e) {
+                        clientList.remove(localSocket);
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
@@ -38,5 +47,21 @@ public class ServerAppInitializer {
         }finally {
             if (serverSocket != null && !serverSocket.isClosed()) serverSocket.close();
         }
+    }
+
+    private static void notifyClients() {
+        new Thread(() -> {
+            for (Socket client : clientList) {
+                try {
+                    OutputStream os = client.getOutputStream();
+                    BufferedOutputStream bos = new BufferedOutputStream(os);
+                    bos.write(messages.getBytes());
+                    bos.flush();
+                } catch (IOException e) {
+                    System.out.println("Failed to notify to the client: " + client);
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
